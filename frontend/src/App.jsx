0de6671a0035d2b10da09c80855bf24a772bd23a
Ibtihal07741 <ibtihal.djezzar@ensia.edu.dz>
2025-11-30 01:23:39 +0100

    updated the routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,30 @@ import Home from './pages/Home';
 import HomeAfterLogIn from './pages/HomeAfterLogIn';
 import AddItem from './pages/AddItem';
 import Profile from './pages/Profile';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
 import WishList from './pages/WishList'
 import EditProfile from './pages/EditProfile';
 import BrowseCategories from './pages/BrowseCategories';
-
+import Messages from './pages/messages';
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
         {/* <Route path="/" element={<WishList />} /> */}
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/home-after-login" element={<HomeAfterLogIn />} />
         <Route path="/add-item" element={<AddItem />} />
         <Route path="/edit-profile" element={<EditProfile />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/Profile" element={<Profile />} />
         <Route path="/wishList" element={<WishList />} />
+        <Route path="/LoginPage" element={<LoginPage />} />
+        <Route path="/SignUpPage" element={<SignUpPage />} />
+        <Route path="/messages" element={<Messages />} />
         <Route path="/browse-categories" element={<BrowseCategories />} />
       </Routes>
     </BrowserRouter>
+
   );
 }
